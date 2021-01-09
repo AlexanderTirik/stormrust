16fed77525ef7ae1026baf9cc53323006a8dab4a
AlexanderTirik <sasha.tirik@gmail.com>
2021-01-10 00:56:32 +0200

    change user type in request

---
+++ backend/src/common/@types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { IAuthUser } from "../../models/user/IAuthUser";
+declare global {
+  namespace Express {
+    interface User extends IAuthUser { }
+
+    interface Request {
+      user?: User;
+    }
+  }
+}
