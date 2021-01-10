99029400d6a53062522a76a83796ca3dc8abfac2
AlexanderTirik <sasha.tirik@gmail.com>
2021-01-10 19:26:01 +0200

    create storage helper

--- frontend/src/common/helpers/storageHelper.ts
+++ frontend/src/common/helpers/storageHelper.ts
@@ -1,1 +1,20 @@
-export const getToken = () => localStorage.getItem('token');
+import { LocalStorageFields } from '../enums/LocalStorageFields';
+import { ITokens } from '../models/fetch/ITokens';
+
+export const setAccessToken = (token: string): void => localStorage.setItem(LocalStorageFields.AccessToken, token);
+
+export const getAccessToken = (): string | null => localStorage.getItem(LocalStorageFields.AccessToken);
+
+export const setRefreshToken = (refreshToken: string) => localStorage.setItem(
+  LocalStorageFields.RefreshToken,
+  refreshToken
+);
+
+export const getRefreshToken = (): string | null => localStorage.getItem(LocalStorageFields.RefreshToken);
+
+export const setTokens = (tokens: ITokens) => {
+  setAccessToken(tokens.accessToken);
+  setRefreshToken(tokens.refreshToken);
+};
+
+export const clearStorage = (): void => localStorage.clear();
